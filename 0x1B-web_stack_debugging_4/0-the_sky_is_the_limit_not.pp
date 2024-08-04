3bf33173e9cb58c56218fbdfc79dada45d7881f0
chingling008 <130581550+chingling008@users.noreply.github.com>
2024-08-04 13:13:59 +0200

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,5 +0,0 @@
-#change ULIMIT
-exec { 'Change ULIMIT':
-  command  => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx && sudo service nginx restart',
-  provider => shell,
-}
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,5 +0,0 @@
-#change ULIMIT
-exec { 'Change ULIMIT':
-  command  => 'echo -e "holberton hard nofile 2500\nholberton soft nofile 25000" > /etc/security/limits.conf',
-  provider => shell,
-}
